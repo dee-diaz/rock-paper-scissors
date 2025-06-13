399f6a4cecb3491af32a95f51cf9fc0812172926
dee-diaz <diazthediablo@yahoo.com>
2025-06-13 18:30:04 -0300

    Add playGame function

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log(
-      `It's a tie! ${humanChoice} VS ${computerChoice}`
-    );
-    return;
+  function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+    if (humanChoice === computerChoice) {
+      console.log(`It's a tie! ${humanChoice} VS ${computerChoice}`);
+      return;
+    }
+
+    const pairs = [
+      {
+        pair: ["rock", "scissors"],
+        winner: "rock",
+      },
+      {
+        pair: ["scissors", "paper"],
+        winner: "scissors",
+      },
+      {
+        pair: ["paper", "rock"],
+        winner: "paper",
+      },
+    ];
+
+    const match = pairs.find((item) => {
+      return (
+        item.pair.includes(humanChoice) && item.pair.includes(computerChoice)
+      );
+    });
+
+    if (match.winner === humanChoice) {
+      humanScore++;
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      console.log(
+        `Your score: ${humanScore}. Computer's score: ${computerScore}`
+      );
+    } else {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      console.log(
+        `Your score: ${humanScore}. Computer's score: ${computerScore}`
+      );
+    }
   }
 
-  const pairs = [
-    {
-      pair: ["rock", "scissors"],
-      winner: "rock",
-    },
-    {
-      pair: ["scissors", "paper"],
-      winner: "scissors",
-    },
-    {
-      pair: ["paper", "rock"],
-      winner: "paper",
-    },
-  ];
-
-  const match = pairs.find((item) => {
-    return item.pair.includes(humanChoice) && item.pair.includes(computerChoice);
-  });
-
-  if (match.winner === humanChoice) {
-    humanScore ++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    console.log(`Your score: ${humanScore}. Computer's score: ${computerScore}`);
-  } else {
-    computerScore ++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    console.log(`Your score: ${humanScore}. Computer's score: ${computerScore}`);
+  function getHumanChoice() {
+    let choice = prompt("Rock, paper or scissors?").toLowerCase();
+    return choice;
   }
+
+  function getComputerChoice() {
+    const choice = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choice[randomIndex];
+  }
+
+  function defineWinner(humanScore, computerScore) {
+    if (humanScore > computerScore) {
+      return `You won.\n Your score: ${humanScore}. Computer's score: ${computerScore}`;
+    } else if (humanScore === computerScore) {
+      return `It's a tie.\n Your score: ${humanScore}. Computer's score: ${computerScore}`;
+    } else {
+      return `Computer won.\n Your score: ${humanScore}. Computer's score: ${computerScore}`;
+    }
+  }
+
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  const result = defineWinner(humanScore, computerScore);
+
+  alert(result);
+  return result;
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-function getHumanChoice() {
-  let choice = prompt("Rock, paper or scissors?").toLowerCase();
-  return choice;
-}
-
-function getComputerChoice() {
-  const choice = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return choice[randomIndex];
-}
-
-playRound(humanSelection, computerSelection);
-
+playGame();
